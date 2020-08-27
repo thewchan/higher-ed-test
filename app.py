14ee80ceda0e9d0da0e93e46f3b60a1531f942e7
Matt Chan <thewchan@gmail.com>
2020-08-27 12:13:16 -0700

    Add map.

--- app.py
+++ app.py
@@ -69,27 +69,51 @@ overall_donations = {school: donations
 school_donations_by_date = [
     namedtuple(
         'donations_by_date',
-        ['school', 'date', 'donation', 'country']
-    )(school, date, donation, country)
-    for school, date, donation, country
+        ['school', 'date', 'donation', 'country', 'giftor']
+    )(school, date, donation, country, giftor)
+    for school, date, donation, country, giftor
     in session.query(
         DonationData.Institution_name,
         DonationData.Foreign_Gift_Received_Date,
         DonationData.Foreign_Gift_Amount,
         DonationData.Country_of_Giftor,
+        DonationData.Giftor_Name,
     ).filter(
         DonationData.Institution_name == 'Stanford University'
     )
 ]
+
+school_donation_map = [
+    namedtuple(
+        'donation_map',
+        ['school', 'date', 'donation', 'country', 'lat', 'lon', 'score']
+    )(school, date, donation, country, lat, lon, score)
+    for school, date, donation, country, lat, lon, score
+    in session.query(
+        DonationData.Institution_name,
+        DonationData.Foreign_Gift_Received_Date,
+        DonationData.Foreign_Gift_Amount,
+        DonationData.Country_of_Giftor,
+        DonationData.Country_Latitude,
+        DonationData.Country_Longitude,
+        DonationData.Score,
+    ).filter(
+        DonationData.Institution_name == 'Stanford University'
+    )
+]
+
 
 df = pd.DataFrame(
     overall_donations.items(),
     columns=['School', 'Amount of Foreign Donations Received']
 )
 
-df2 = pd.DataFrame(school_donations_by_date)
+df2 = pd.DataFrame(school_donations_by_date).fillna('Unknown')
 df2['date'] = pd.to_datetime(df2['date'])
 df2 = df2.sort_values('date')
+
+df3 = pd.DataFrame(school_donation_map).fillna('Unknown')
+# df3['date'] = pd.to_datetime(df3['date'])
 
 fig = px.bar(
     df,
@@ -104,13 +128,28 @@ fig2 = px.scatter(
     df2,
     x='date',
     y='donation',
-    hover_data=['country']
+    hover_data=['country', 'giftor']
 )
 fig2.update_xaxes(title_text='')
 fig2.update_yaxes(
     title_text='',
     tickprefix="$"
 )
+
+# px.set_mapbox_access_token(open(".mapbox_token").read())
+fig3 = px.scatter_mapbox(
+    df3,
+    lat="lat",
+    lon="lon",
+    color="score",
+    size="donation",
+    color_continuous_scale=px.colors.sequential.Bluered[::-1],
+    # reversescale=True,
+    size_max=15,
+    zoom=0,
+    hover_data=['country', 'date'],
+    )
+fig3.update_layout(mapbox_style="open-street-map")
 
 app.layout = html.Div(children=[
     html.Div(
@@ -131,7 +170,7 @@ app.layout = html.Div(children=[
             html.Div(
                 children=dcc.Graph(
                     id='example-graph3',
-                    figure=fig
+                    figure=fig3
                 ),
                 className='one-half column'
             )
