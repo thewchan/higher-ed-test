7181e97f39171fddd33fe4a29f91dbbffdce972d
Matt Chan <thewchan@gmail.com>
2020-08-27 21:14:05 -0700

    Refactor figure making to a separate mordule.

--- app.py
+++ app.py
@@ -2,9 +2,14 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.express as px
-from gen_dataframe import (get_donations_df, get_donationsMAP_df,
+
+from gen_dataframe import (get_donations_df,
+                           get_donationsMAP_df,
                            get_donationsTS_df)
+from gen_figures import (get_donation_bar,
+                         get_donation_map,
+                         get_donationTS_scatter)
+
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -12,59 +17,13 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 db_path = 'sqlite:///merged_data_w_coord.sqlite'
 
-df = get_donations_df(db_path)
-df2 = get_donationsTS_df(db_path, 'Carnegie Mellon University')
-df3 = get_donationsMAP_df(db_path, 'Carnegie Mellon University')
+overall_donations_df = get_donations_df(db_path)
+donationTS_df = get_donationsTS_df(db_path, 'Carnegie Mellon University')
+donation_map_df = get_donationsMAP_df(db_path, 'Carnegie Mellon University')
 
-fig = px.bar(
-    df,
-    x="Amount",
-    y="School",
-    orientation='h',
-)
-fig.update_yaxes(title_text='')
-fig.update_xaxes(tickprefix='$')
-
-fig2 = px.scatter(
-    df2,
-    x='Date',
-    y='Amount',
-    custom_data=['Donor Country', 'Donor'],
-    color_discrete_sequence=px.colors.qualitative.Alphabet,
-    color='Donor Country'
-)
-fig2.update_xaxes(title_text='')
-fig2.update_yaxes(
-    title_text='',
-    tickprefix="$"
-)
-fig2.update_traces(
-    hovertemplate=("Date: %{x|%Y-%m-%d}<br>"
-                   "Amount: %{y:$,}<br>"
-                   "Donor Country: %{customdata[0]}<br>"
-                   "Donor: %{customdata[1]}")
-)
-
-# px.set_mapbox_access_token(open(".mapbox_token").read())
-fig3 = px.scatter_mapbox(
-    df3,
-    lat="Latitude",
-    lon="Longitude",
-    color="Score",
-    size="Amount",
-    custom_data=['Amount', 'Score', 'Date', 'Donor Country'],
-    color_continuous_scale=px.colors.sequential.Bluered[::-1],
-    opacity=0.5,
-    size_max=15,
-    zoom=0,
-)
-fig3.update_layout(mapbox_style="open-street-map")
-fig3.update_traces(
-    hovertemplate=("Donor Country: %{customdata[3]}<br>"
-                   "Date: %{customdata[2]|%Y-%m-%d}<br>"
-                   "Amount: %{customdata[0]:$,}<br>"
-                   "Democracy Score: %{customdata[1]}")
-)
+donation_bar = get_donation_bar(overall_donations_df)
+donationTS_scatter = get_donationTS_scatter(donationTS_df)
+donation_map = get_donation_map(donation_map_df)
 
 app.layout = html.Div(children=[
     html.Div(
@@ -78,14 +37,14 @@ app.layout = html.Div(children=[
             html.Div(
                 children=dcc.Graph(
                     id='example-graph2',
-                    figure=fig2
+                    figure=donationTS_scatter
                 ),
                 className='one-half column'
             ),
             html.Div(
                 children=dcc.Graph(
                     id='example-graph3',
-                    figure=fig3
+                    figure=donation_map
                 ),
                 className='one-half column'
             )
@@ -95,12 +54,11 @@ app.layout = html.Div(children=[
     html.Div(
         children=dcc.Graph(
             id='example-graph',
-            figure=fig,
+            figure=donation_bar,
         ),
         className='row'
     )
-]
-)
+])
 
 
 if __name__ == '__main__':
---
+++ gen_figures.py
@@ -0,0 +1,66 @@
+"""Helper functions to generate figures."""
+import plotly.express as px
+
+
+def get_donation_bar(data):
+    """Return the master horizontal bar graph."""
+    fig = px.bar(
+        data,
+        x="Amount",
+        y="School",
+        orientation='h',
+    )
+    fig.update_yaxes(title_text='')
+    fig.update_xaxes(tickprefix='$')
+
+    return fig
+
+
+def get_donationTS_scatter(data):
+    """Return scatter plot of the donation time series."""
+    fig = px.scatter(
+        data,
+        x='Date',
+        y='Amount',
+        custom_data=['Donor Country', 'Donor'],
+        color_discrete_sequence=px.colors.qualitative.Alphabet,
+        color='Donor Country'
+    )
+    fig.update_xaxes(title_text='')
+    fig.update_yaxes(
+        title_text='',
+        tickprefix="$"
+    )
+    fig.update_traces(
+        hovertemplate=("Date: %{x|%Y-%m-%d}<br>"
+                       "Amount: %{y:$,}<br>"
+                       "Donor Country: %{customdata[0]}<br>"
+                       "Donor: %{customdata[1]}")
+    )
+
+    return fig
+
+
+def get_donation_map(data):
+    """Return donation map."""
+    fig = px.scatter_mapbox(
+        data,
+        lat="Latitude",
+        lon="Longitude",
+        color="Score",
+        size="Amount",
+        custom_data=['Donor Country', 'Date', 'Amount', 'Score'],
+        color_continuous_scale=px.colors.sequential.Bluered[::-1],
+        opacity=0.5,
+        size_max=15,
+        zoom=0,
+    )
+    fig.update_layout(mapbox_style="open-street-map")
+    fig.update_traces(
+        hovertemplate=("Donor Country: %{customdata[0]}<br>"
+                       "Date: %{customdata[1]|%Y-%m-%d}<br>"
+                       "Amount: %{customdata[2]:$,}<br>"
+                       "Democracy Score: %{customdata[3]}")
+    )
+
+    return fig
