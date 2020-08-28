566602a7963d5e2b493c13c8ecd27bc03f0ea937
Matt Chan <thewchan@gmail.com>
2020-08-28 12:24:29 -0700

    Add dropdown menu.

--- app.py
+++ app.py
@@ -5,28 +5,36 @@ import re
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
 from dash.dependencies import Input, Output
 from gen_dataframe import (get_donations_df, get_donationsMAP_df,
                            get_donationsTS_df)
 from gen_figures import (get_donation_bar, get_donation_map,
                          get_donationTS_scatter)
 
+# Global scope variables
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 db_path = 'sqlite:///merged_data_w_coord.sqlite'
 
+with open('./school_abbrev.json', 'r') as f:
+    school_abbrev = json.load(f)
+school_abbrev = pd.DataFrame(
+    school_abbrev.items(),
+    columns=['School', 'Abbrev'])
+
+# Default data when app first load
 overall_donations_df = get_donations_df(db_path)
 donationTS_df = get_donationsTS_df(db_path, 'Carnegie Mellon University')
 donation_map_df = get_donationsMAP_df(db_path, 'Carnegie Mellon University')
-
 donation_bar = get_donation_bar(overall_donations_df)
 donationTS_scatter = get_donationTS_scatter(donationTS_df,
                                             'Carnegie Mellon University')
 donation_map = get_donation_map(donation_map_df,
                                 'Carnegie Melon University')
 
+# Dashboard layout
 app.layout = html.Div(children=[
     html.Div(
         children=html.H1(
@@ -38,14 +46,14 @@ app.layout = html.Div(children=[
         children=[
             html.Div(
                 children=dcc.Graph(
-                    id='example-graph2',
+                    id='donationTS',
                     figure=donationTS_scatter
                 ),
                 className='one-half column'
             ),
             html.Div(
                 children=dcc.Graph(
-                    id='example-graph3',
+                    id='donation-map',
                     figure=donation_map,
                     clickData={
                         'points': [{'y': 'Carnegie Mellon University'}]
@@ -57,16 +65,28 @@ app.layout = html.Div(children=[
         className='row'
     ),
     html.Div(
+        children=dcc.Dropdown(
+            id='school-dropdown',
+            options=[
+                {'label': school, 'value': abbrev}
+                for school, abbrev
+                in school_abbrev.set_index('School')['Abbrev'].items()
+            ],
+            value='cmu'
+        )
+    ),
+    html.Div(
         children=dcc.Graph(
-            id='example-graph',
+            id='master-bar-graph',
             figure=donation_bar,
-            clickData={'points': [{'y': 'Carnegie Mellon University'}]}
+            clickData={'points': [{'y': 'Carnegie Mellon University'}]},
+            selectedData=None,
         ),
         className='row',
     ),
     html.Div(
         children=html.Pre(
-            id='test-click',
+            id='debug',
             style={
                 'border': 'thin lightgrey solid',
                 'overflowX': 'scroll'
@@ -77,8 +97,8 @@ app.layout = html.Div(children=[
 
 
 @app.callback(
-    Output('example-graph2', 'figure'),
-    [Input('example-graph', 'clickData')]
+    Output('donationTS', 'figure'),
+    [Input('master-bar-graph', 'clickData')]
 )
 def update_donationTS_scatter(clickData):
     school_re = re.compile(r'[^\"]')
@@ -91,10 +111,10 @@ def update_donationTS_scatter(clickData):
 
 
 @app.callback(
-    Output('example-graph3', 'figure'),
-    [Input('example-graph', 'clickData')]
+    Output('donation-map', 'figure'),
+    [Input('master-bar-graph', 'clickData')]
 )
-def update_donationTS_scatter(clickData):
+def update_donation_map(clickData):
     school_re = re.compile(r'[^\"]')
     school = ''.join(
         school_re.findall(json.dumps(clickData['points'][0]['y']))
@@ -102,6 +122,38 @@ def update_donationTS_scatter(clickData):
     new_donationsMap_df = get_donationsMAP_df(db_path, school)
     figure = get_donation_map(new_donationsMap_df, school)
     return figure
+
+
+@app.callback(
+    Output('master-bar-graph', 'clickData'),
+    [Input('school-dropdown', 'value')]
+)
+def update_clickData_master(value):
+    school = (school_abbrev[school_abbrev['Abbrev'] == value]
+              ['School'].values[0])
+    clickData = {'points': [{'y': school}]}
+
+    return clickData
+
+
+@app.callback(
+    Output('master-bar-graph', 'selectedData'),
+    [Input('school-dropdown', 'value')]
+)
+def update_selectedData_master(value):
+    school = (school_abbrev[school_abbrev['Abbrev'] == value]
+              ['School'].values[0])
+    selectedData = {'points': [{'y': school}]}
+
+    return selectedData
+
+
+@app.callback(
+    Output('debug', 'children'),
+    [Input('master-bar-graph', 'selectedData')]
+)
+def selected_data_debug(selectedData):
+    return json.dumps(selectedData)
 
 
 if __name__ == '__main__':
